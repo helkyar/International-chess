849f4cb520c8bb7a025699d44a543429d8da4457
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-02-19 14:29:11 +0100

    Innit models and managers

---
+++ src/onlinechess/controller/socket/Package.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package onlinechess.controller.socket;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ *
+ * @author Javier Palacios
+ */
+public class Package implements Serializable{
+    
+    private String nick, ip, info, msg, status;
+    private Map<String, String> ips, users;
+
+    public void setIp(String ip){this.ip = ip;}
+    public void setMsg(String msg){this.msg = msg;}
+    public void setNick(String nick){this.nick = nick;}
+    public void setInfo(String info){this.info = info;}
+    public void setStatus(String status){this.status = status;}
+    public void setIps(Map<String, String> ips){this.ips = ips;}
+    public void setUsers(Map<String, String> users){this.users = users;}
+    
+    public String getNick(){return nick;}
+    public String getIp(){return ip;}
+    public String getInfo(){return info;}
+    public String getMsg(){return msg;}
+    public String getStatus(){return status;}
+    public Map<String,String> getIps(){return ips;}
+    public Map<String, String> getUsers() {return users;}
+}
